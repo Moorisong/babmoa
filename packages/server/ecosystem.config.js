5edd6d33d985d1bdc4e4708f412e89ef86ee7db9
Kim SongHyeon <thiagoo@naver.com>
2025-12-28 14:33:23 +0900

    fix: PM2 절대경로로 변경하여 .env 로드 문제 해결
    - script와 env_file을 절대경로로 변경
    - PM2가 상대경로로 .env를 찾지 못해 MONGO_URI undefined 발생
    - 이로 인해 mongoose가 localhost:27017 접속 시도하여 ECONNREFUSED 에러

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
         {
             name: "babmoa",
-            script: "src/index.js",
-            env_file: ".env",
+            script: "/home/ksh/babmoa/packages/server/src/index.js",
+            env_file: "/home/ksh/babmoa/packages/server/.env",
             instances: 1,
             autorestart: true,
             watch: false
